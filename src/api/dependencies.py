2f9b74970e0aefb545f54554b4c24185635c8630
MetaMaxfield <metamaxfield@gmail.com>
2025-11-17 17:02:47 +0400

    fix: async syntax

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,7 +1,6 @@
-from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import async_session_maker
 
 
-async def get_db_session() -> AsyncSession:
-    with async_session_maker() as session:
+async def get_db_session():
+    async with async_session_maker() as session:
         yield session
