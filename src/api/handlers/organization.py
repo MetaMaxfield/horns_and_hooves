17a66756db6b3cfa94a64636bc07e96bfd90bebb
MetaMaxfield <metamaxfield@gmail.com>
2025-11-17 19:52:15 +0400

    feature: organizations in radius

--- src/api/handlers/organization.py
+++ src/api/handlers/organization.py
@@ -1,12 +1,12 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from sqlalchemy import cast, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import contains_eager, joinedload, selectinload
-from geoalchemy2 import functions as geo_func
+from geoalchemy2 import Geography, functions as geo_func
 
 from src.api.dependencies import get_db_session
-from src.api.schemas.geo import Rectangle
+from src.api.schemas.geo import Circle, Rectangle
 from src.api.schemas.organization import OrganizationRead
 from src.repo.building.models import Building
 from src.repo.organization.models import Organization
@@ -15,12 +15,37 @@ from src.repo.organization.models import Organization
 router = APIRouter(prefix="/organizations")
 
 
+@router.get("/within-radius")
+async def organizations_in_radius(
+    circle: Annotated[Circle, Query()],
+    session: AsyncSession = Depends(get_db_session),
+) -> list[OrganizationRead]:
+    center_point = geo_func.ST_SetSRID(
+        geo_func.ST_MakePoint(circle.lon, circle.lat), 4326
+    )
+    query = (
+        select(Organization)
+        .join(Organization.building)
+        .filter(
+            geo_func.ST_DWithin(
+                cast(Building.geom, Geography),
+                cast(center_point, Geography),
+                circle.radius,
+            )
+        )
+        .options(contains_eager(Organization.building))
+        .options(selectinload(Organization.phone_numbers))
+        .options(selectinload(Organization.activities))
+    )
+    orgs = await session.execute(query)
+    return orgs.scalars().all()
+
+
 @router.get("/within-rectangle")
 async def organizations_in_area(
     rectangle: Annotated[Rectangle, Query()],
     session: AsyncSession = Depends(get_db_session),
 ) -> list[OrganizationRead]:
-    """Организации в области."""
     envelope = geo_func.ST_MakeEnvelope(
         rectangle.lon_min, rectangle.lat_min, rectangle.lon_max, rectangle.lat_max, 4326
     )
