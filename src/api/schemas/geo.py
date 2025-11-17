e2f29922af9b555a202902edaabb6f74307bcdcb
MetaMaxfield <metamaxfield@gmail.com>
2025-11-17 19:51:41 +0400

    feature: circle schema

--- src/api/schemas/geo.py
+++ src/api/schemas/geo.py
@@ -17,3 +17,15 @@ class Rectangle(BaseModel):
     )
 
     model_config = ConfigDict(extra="forbid", from_attributes=True)
+
+
+class Circle(BaseModel):
+    lat: float = Field(
+        ge=c.BUILDING_LATITUDE_MIN_VALUE, le=c.BUILDING_LATITUDE_MAX_VALUE
+    )
+    lon: float = Field(
+        ge=c.BUILDING_LONGITUDE_MIN_VALUE, le=c.BUILDING_LONGITUDE_MAX_VALUE
+    )
+    radius: int = Field(ge=c.MIN_SEARCH_RADIUS, le=c.MAX_SEARCH_RADIUS)
+
+    model_config = ConfigDict(extra="forbid", from_attributes=True)
