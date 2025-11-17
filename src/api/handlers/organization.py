5048a1f3b0abcbe3d5c35ff9d5a93f80fe788b18
MetaMaxfield <metamaxfield@gmail.com>
2025-11-17 19:37:38 +0400

    feature: organizations in area

--- src/api/handlers/organization.py
+++ src/api/handlers/organization.py
@@ -1,14 +1,39 @@
-from fastapi import APIRouter, Depends, HTTPException
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload, selectinload
+from sqlalchemy.orm import contains_eager, joinedload, selectinload
+from geoalchemy2 import functions as geo_func
 
 from src.api.dependencies import get_db_session
+from src.api.schemas.geo import Rectangle
 from src.api.schemas.organization import OrganizationRead
+from src.repo.building.models import Building
 from src.repo.organization.models import Organization
 
 
 router = APIRouter(prefix="/organizations")
+
+
+@router.get("/within-rectangle")
+async def organizations_in_area(
+    rectangle: Annotated[Rectangle, Query()],
+    session: AsyncSession = Depends(get_db_session),
+) -> list[OrganizationRead]:
+    """Организации в области."""
+    envelope = geo_func.ST_MakeEnvelope(
+        rectangle.lon_min, rectangle.lat_min, rectangle.lon_max, rectangle.lat_max, 4326
+    )
+    query = (
+        select(Organization)
+        .join(Organization.building)
+        .filter(geo_func.ST_Within(Building.geom, envelope))
+        .options(contains_eager(Organization.building))
+        .options(selectinload(Organization.phone_numbers))
+        .options(selectinload(Organization.activities))
+    )
+    orgs = await session.execute(query)
+    return orgs.scalars().all()
 
 
 @router.get("/by-name")
