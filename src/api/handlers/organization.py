c4231ded73011e37e90ba613703d7e37dbb5125b
MetaMaxfield <metamaxfield@gmail.com>
2025-11-17 17:55:13 +0400

    feature: organizations by name

--- src/api/handlers/organization.py
+++ src/api/handlers/organization.py
@@ -11,6 +11,21 @@ from src.repo.organization.models import Organization
 router = APIRouter(prefix="/organizations")
 
 
+@router.get("/by-name")
+async def organizations_by_name(
+    organization_name: str, session: AsyncSession = Depends(get_db_session)
+) -> list[OrganizationRead]:
+    query = (
+        select(Organization)
+        .options(selectinload(Organization.phone_numbers))
+        .options(selectinload(Organization.activities))
+        .options(joinedload(Organization.building))
+        .filter(Organization.name.ilike(f"%{organization_name}%"))
+    )
+    orgs = await session.execute(query)
+    return orgs.scalars().all()
+
+
 @router.get("/{organization_id}")
 async def organization_by_id(
     organization_id: int, session: AsyncSession = Depends(get_db_session)
