14ac4b45f9952865ea64a1b35167bfab989e9056
MetaMaxfield <metamaxfield@gmail.com>
2025-11-18 23:00:33 +0400

    feature: new dependencies

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,6 +1,9 @@
-from fastapi import HTTPException, Header, status
+from fastapi import Depends, HTTPException, Header, status
+from sqlalchemy.ext.asyncio import AsyncSession
 from src.config import settings
 from src.database import async_session_maker
+from src.repo.organization.repo import OrganizationRepo
+from src.usecase.organization import OrganizationUseCase
 
 
 async def verify_api_key(x_api_key: str = Header(alias="X-API-Key")):
@@ -13,3 +16,13 @@ async def verify_api_key(x_api_key: str = Header(alias="X-API-Key")):
 async def get_db_session():
     async with async_session_maker() as session:
         yield session
+
+
+async def get_organization_repo(session: AsyncSession = Depends(get_db_session)):
+    return OrganizationRepo(session)
+
+
+async def get_organization_use_case(
+    org_repo: OrganizationRepo = Depends(get_organization_repo),
+):
+    return OrganizationUseCase(org_repo)
